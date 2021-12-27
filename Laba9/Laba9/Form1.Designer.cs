0bd75c405aa44c0d6684e9b15e862736ab9ce482
Arseniy1315 <90174473+Arseniy1315@users.noreply.github.com>
2021-12-27 21:56:15 +0700

    Add files via upload

---
+++ Laba9/Laba9/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Laba9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(47, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(247, 194);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(348, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Снять деньги";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(541, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Положить на счет";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(539, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(348, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 24);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Снять деньги";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(348, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(345, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ввидите номер аккаунт";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Laba9/Laba9/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum Score_Type
+        {
+            Текущий, Сберегательный
+        }
+
+        public class BankAccount
+        {
+            private int  Account_Number;
+            private double Balance;
+            private Score_Type type;
+            private static int AccountID=1;
+
+            public BankAccount()
+            {
+                Account_Number = AccountID;
+                NumberAccount();
+            }
+            public int GetNumber()
+            {
+                return Account_Number;
+            }
+
+
+            public double GetBalance()
+            {
+                return Balance;
+            }
+            public Score_Type Gettype()
+            {
+                return type;
+            }
+
+            public void Type(Score_Type type2)
+            {
+                type = type2;
+            }
+
+            public void Total_Balance(int money)
+            {
+                Balance = money;
+            }
+
+
+            public static void NumberAccount()
+            {
+                AccountID++;
+            }
+            public void WithdrawMoney(int num, int take)
+            {
+                
+
+                if (BankAccount.AccountID == num)
+                {
+                    if ( Balance >= take)
+                    {
+                        Balance = Balance - take;
+                        MessageBox.Show($"Со счета была снята сумма {Balance}");
+                    }
+                    else
+                    {
+                        MessageBox.Show("На счету не достаточно средств");
+                    }
+
+                }
+
+
+
+                //if (BankAccount.AccountID == BankAccount.AccountID )
+                //{
+            //    if (Balance >= take)
+            //    {
+            //        Balance = Balance - take;
+            //        MessageBox.Show($"Со счета была снята сумма {Balance}");
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("На счету не достаточно средств");
+            //    }
+            //}
+
+
+        }
+        public void Deposit_Money()
+            {
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.Total_Balance(rnd.Next(10000, 1000000));
+            Score_Type type = bankAccount.Gettype();
+            richTextBox1.Text = $"Аккаунт {bankAccount.GetNumber()} счет {type}  баланс {bankAccount.GetBalance()}  \n";
+            BankAccount bank = new BankAccount();
+            bank.Total_Balance(rnd.Next(12002, 213213));
+            bank.Type(Score_Type.Сберегательный);
+            Score_Type type1 = bank.Gettype();
+            richTextBox1.Text += $"Аккаунт {bank.GetNumber()} cчет {type1} баланс {bank.GetBalance()} \n";
+
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
